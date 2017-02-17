1d58d54abab1a715126b6f930e1d3e7031adfe2f
Alexandre Lopoukhine <superlopuh@gmail.com>
2017-02-17 14:45:37 +0100

    add empty cross-platform project and workspace

---
+++ Sources/SuperCombinators.h
@@ -0,0 +1,15 @@
+//
+//  SuperCombinators.h
+//  SuperCombinators
+//
+//  Created by Alexandre Lopoukhine on 17/02/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SuperCombinators.
+FOUNDATION_EXPORT double SuperCombinatorsVersionNumber;
+
+//! Project version string for SuperCombinators.
+FOUNDATION_EXPORT const unsigned char SuperCombinatorsVersionString[];
